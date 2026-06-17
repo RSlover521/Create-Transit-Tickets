7465b6fa5bae356f8ecc3e8039b850de3198f92f
RSlover521 <wulance58@gmail.com>
2026-06-17 08:18:49 -0500

    added forge MDK for development

---
+++ src/main/java/net/rslover521/createTransitTickets/CreateTransitTickets.java
@@ -0,0 +1,31 @@
+package net.rslover521.createTransitTickets;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CreateTransitTickets.MODID)
+public class CreateTransitTickets {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "create_transit_tickets";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateTransitTickets(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+}
